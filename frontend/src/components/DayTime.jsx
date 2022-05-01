f20d72ee8c815e0b4f8a71b49b1cc60ec027e82a
rodrigue <rodrigue_techer@yahoo.com>
2022-05-01 10:35:34 +0200

    ajout du composant daytime heure en fixe

--- frontend/src/components/DayTime.jsx
+++ frontend/src/components/DayTime.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import "../css/DayTime.css";
+import DayJS from "react-dayjs";
 
 function DayTime() {
-  return <div>DayTime</div>;
+  function clock() {
+    const date = new Date();
+    const hours = ((date.getHours() + 11) % 12) + 1;
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    return `${hours} H ${minutes} M ${seconds}`;
+  }
+  return (
+    <div className="dayTime">
+      <h1 className="dayTime_title">Parking Nantais</h1>
+      <p className="dayTime_nom">Nombre de place libre</p>
+      <p>{clock()};</p>
+    </div>
+  );
 }
 
 export default DayTime;
